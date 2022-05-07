ea0b6ea0b1749db3112e4668a04ac0c2bdb75fca
jeannoel <nfonjeannoel1@gmail.com>
2022-05-07 13:15:19 +0100

    initial commit

---
+++ assets/js/theme.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.datepicker').forEach(function (field) {
+    var picker = new Pikaday({
+        field: field
+    });
+});
+
+// var myModal = document.getElementById('modalContactForm')
+// var myInput = document.getElementById('form34')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//     myInput.focus()
+// })
